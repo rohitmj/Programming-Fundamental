d30a2ab21c950a7f620326eba1b2ff9d5bbf9cb0
rohitmj <rohit.maharjan619@gmail.com>
2016-11-16 13:19:32 +0545

    Add files via upload

---
+++ rohitmj.c
@@ -0,0 +1,22 @@
+/*
+student name: rohit maharjan
+subject : programming fundamentals
+roll no:
+sheet :
+program : write a program to print the area of triangle
+date: 16 november 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,area;
+printf("enter the base:\n",a);
+scanf("%d",&a);
+printf("enter the height:\n",b);
+scanf("%d",&b);
+area=0.5*a*b;
+printf("area of triangle is:%d\n",area);
+getch();
+return 0;
+}

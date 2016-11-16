ad37c8d24a7c4a67e4abfecd61f286cb42015db9
rohitmj <rohit.maharjan619@gmail.com>
2016-11-16 13:06:22 +0545

    Add files via upload

---
+++ rohi.c
@@ -0,0 +1,25 @@
+
+/*
+student name : Rohit Maharjan
+subject : programming fundamental
+roll no :
+sheet :
+program :write a c program to print the sum of two numbers
+date : 16 November 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,sum;
+
+
+printf("Enter the first number:\n",a);
+scanf("%d",&a);
+ printf("enter the second number:\n");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("sum of two number is:%d\n",sum);
+getch();
+return 0;
+}

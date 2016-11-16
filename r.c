4fa7fc97eb70800d4386ec62c2fae2e7e11e2545
rohitmj <rohit.maharjan619@gmail.com>
2016-11-16 10:59:52 +0545

    Add files via upload

---
+++ r.c
@@ -0,0 +1,18 @@
+/*
+student name : rohit maharjan
+subject : programming fundamental
+roll no :
+sheet no :
+program : write a c program to print name ask from user
+date : 16 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+char name[5];
+printf("what is your name\n");
+scanf("%s",&name);
+printf("my name is:%s",name);
+getch();
+return 0;
+}

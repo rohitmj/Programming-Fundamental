15e6cffc21e054534e34a60b075596759003431b
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:43:04 +0545

    Update print the name.c

--- print the name.c
+++ print the name.c
@@ -1,18 +1,18 @@
 /*
-student name : rohit maharjan
-subject : programming fundamental
-roll no :
-sheet no :
-program : write a c program to print name asked from the user
+student name : Rohit Maharjan
+subject : Programming Fundamental
+roll no :23
+sheet no :1
+program : Write a c program to print name asked from the user
 date : 16 November 2016
 */
 #include<stdio.h>
 #include<conio.h>
 int main (){
 char name[5];
-printf("what is your name\n");
+printf("What is your name\n");
 scanf("%s",&name);
-printf("my name is:%s",name);
+printf("My name is:%s",name);
 getch();
 return 0;
 }

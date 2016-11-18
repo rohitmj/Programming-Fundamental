7a1e29d76f4b8fd5ec17e41c8fdfa4e4d34a089c
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:45:26 +0545

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -2,7 +2,7 @@
 student name: Rohit Maharjan
 subject: Computer fundamentals
 roll no:
-programe : write a c program to print the subtract of two numbers given by the user
+programe : Write a c program to print the subtract of two numbers given by the user
 date:18 November,2016
 */
 #include<stdio.h>
@@ -10,13 +10,12 @@ date:18 November,2016
 int main()
 {
 int a,b,subtract;
-printf("enter the first number:\n");
+printf("Enter the first number:\n");
 scanf("%d",&a);
-printf("enter the second number:\n");
+printf("Enter the second number:\n");
 scanf("%d",&b);
 subtract=a-b;
-printf("the subtraction of two numbers given by the user is:%d\n",subtract);
-
+printf("The subtraction of two numbers given by the user is:%d\n",subtract);
 getch();
 return 0;
 }

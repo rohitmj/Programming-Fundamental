9363a5050a71235a774b9cee7561d3d5a08eb63b
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:47:24 +0545

    Update users name.c

--- users name.c
+++ users name.c
@@ -1,18 +1,18 @@
 /*
-student name : rohit maharjan
+student name : Rohit Maharjan
 subject : programming fundamental
-roll no :
-sheet no :
-program : write a c program to print name ask from user
+roll no :23
+sheet no :1
+program : Write a c program to print name ask from user
 date : 16 November 2016
 */
 #include<stdio.h>
 #include<conio.h>
 int main (){
 char name;
-printf("what is your name");
+printf("What is your name");
 scanf("%s",& name);
-printf(" name of the user is:%s",name);
+printf("Name of the user is:%s",name);
 getch();
 return 0;
 }

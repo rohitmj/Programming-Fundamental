f0a02ccbba07e417b9680795397d0baa55643926
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:31:18 +0545

    Add files via upload

---
+++ area and circumference of circle.c
@@ -0,0 +1,21 @@
+/*
+student name: rohit maharjan
+subject : programming fundamentals
+roll no:
+sheet :
+program : write a c program to calculate the area and circumference of circles having radius r. r should be taken from the user
+date: 18 november 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+float r,area,circumference;
+printf("enter the radius:\n");
+scanf("%f",&r);
+area=3.14*r*r;
+circumference=2*3.14*r;
+printf(" area of the circle is:%f\n",area);
+printf("circumference of the circle is:%f\n",circumference);
+getch();
+return 0;
+}

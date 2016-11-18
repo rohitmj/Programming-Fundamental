90d26527c8d207dd77d4f8e88ad836472d59ab64
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:36:13 +0545

    Update area of triangle.c

--- area of triangle.c
+++ area of triangle.c
@@ -1,22 +1,22 @@
 /*
-student name: rohit maharjan
-subject : programming fundamentals
-roll no:
-sheet :
-program : write a program to print the area of triangle
-date: 16 november 2016
+student name: Rohit Maharjan
+subject : Programming Fundamentals
+roll no:23
+sheet :2
+program : Write a program to print the area of triangle
+date: 16 November 2016
 */
 
 #include<stdio.h>
 #include<conio.h>
 int main(){
 int a,b,area;
-printf("enter the base:\n",a);
+printf("Enter the base:\n",a);
 scanf("%d",&a);
-printf("enter the height:\n",b);
+printf("Enter the height:\n",b);
 scanf("%d",&b);
 area=0.5*a*b;
-printf("area of triangle is:%d\n",area);
+printf("Area of triangle is:%d\n",area);
 getch();
 return 0;
 }

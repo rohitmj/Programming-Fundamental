d2491b1d9a3e2eeffec43a6bcd227d5f5aee26db
rohitmj <rohit.maharjan619@gmail.com>
2016-11-18 12:39:06 +0545

    Update name.c

--- name.c
+++ name.c
@@ -1,3 +1,11 @@
+/*
+student name: Rohit Maharjan
+subject : Programming Fundamentals
+roll no:
+sheet :
+program : Write a c program to print your name
+date: 16 november 2016
+*/
 #include<stdio.h>
 #include<conio.h>
 int main(){

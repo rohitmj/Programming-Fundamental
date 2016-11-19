ea8fd766f2f96ce425bd6de4482ff515dfe4db3e
rohitmj <rohit.maharjan619@gmail.com>
2016-11-19 19:49:26 +0545

    Update simple interest.c

--- simple interest.c
+++ simple interest.c
@@ -1,14 +1,13 @@
 /*
-student name: Rohit Maharjan
-subject: Computer fundamentals
-roll no:
-programe : Write a c program to print the simple interest
-date:18 November,2016
+Student name: Rohit Maharjan
+Subject: Computer fundamentals
+Roll no:23
+Programe : Write a c program to print the simple interest
+Date:18 November,2016
 */
 #include<stdio.h>
 #include<conio.h>
-int main()
-{
+int main(){
 float P,T,R,I;
 printf("Enter the principle:\n");
 scanf("%f",&P);
@@ -19,5 +18,4 @@ scanf("%f",&R);
 I=P*T*R/100;
 printf("The simple interest is:%f\n",I);
 getch();
-return 0;
-}
+return 0;}

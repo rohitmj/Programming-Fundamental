a05a4bf7109768b321ac276740c38c37a867861a
rohitmj <rohit.maharjan619@gmail.com>
2016-11-21 10:43:43 +0545

    Add files via upload

---
+++ area of traingle.c
@@ -0,0 +1,26 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer fundamentals
+Roll no:23
+Programe : Write a c program to print the area of triangle
+Lab Sheet No:03
+Date: November,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+float a,b,c,S,Area,e;
+printf("Enter the fist side:\n");
+scanf("%f",&a);
+printf("Enter the second side:\n",b);
+scanf("%f",&b);
+printf("Enter the third side:\n",c);
+scanf("%f",&c);
+S=(a+b+c)/2;
+e=S*(S-a)*(S-b)*(S-c);
+Area=pow(e,0.5);
+printf("The area of traingle is:%f",Area);
+getch();
+ return 0;}

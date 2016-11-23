17acfeffc7a74124adbd1e471185d671460d931a
rohitmj <rohit.maharjan619@gmail.com>
2016-11-23 12:38:41 +0545

    Add files via upload

---
+++ ASCII value.c
@@ -0,0 +1,19 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer fundamentals
+Roll no:23
+Programe : Write a program to find the roots of quadratic equation and value are assigned from user
+Lab Sheet No:04
+Date:23 November,2016
+*/
+#include<stdio.h>
+int main(){
+char character;
+int ASCIIvalue;
+printf("Enter the character:\n");
+scanf("%c",&character);
+ASCIIvalue=character;
+printf("The value of ASCII is %d",ASCIIvalue);
+getch();
+return 0;
+}

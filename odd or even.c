019da2b7c7031571507ea24795c267eaa1e25662
rohitmj <rohit.maharjan619@gmail.com>
2016-11-25 10:40:14 +0545

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,22 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer Fundamentals
+Roll no:23
+Program : Write a c program to check whether the number is odd or even
+Lab Sheet No:04
+Date: 25 November,2016
+*/
+#include<stdio.h>
+int main(){
+int n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("no is even\n");
+}
+else
+{
+printf("no is odd");
+}
+return 0;}

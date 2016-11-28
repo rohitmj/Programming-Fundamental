37ea34345cbe35a1dd7960ece87c5edcb52b2983
rohitmj <rohit.maharjan619@gmail.com>
2016-11-28 12:33:34 +0545

    Add files via upload

---
+++ Square.c
@@ -0,0 +1,21 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer Fundamentals
+Roll no:23
+Program : Write a program to enter any number and calculate its square.
+Lab Sheet No:06
+Date:28 November,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int n,sq;
+    printf("Enter the Number:\n");
+    scanf("%d",&n);
+    sq=pow(n,2);
+    printf("Square root of the given number is:%d",sq);
+    getch();
+    return 0;
+}

a8bef742e699312ba095254dad93eb26ccd31164
rohitmj <rohit.maharjan619@gmail.com>
2016-12-21 21:37:18 +0545

    Add files via upload

---
+++ Enter your name 10 times using while loop.c
@@ -0,0 +1,21 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer fundamentals
+Roll no:23
+Program : Write a program to find the even numbers from 1 to 100 using for loop
+Lab Sheet No:04
+Date:20 December,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x=1;
+    while(x<=10)
+    {
+        printf("Rohit MJ\t");
+        x++;
+    }
+    getch();
+    return 0;
+}

51e9f93f9e41a2d2a30036b3b21f65de3a91ac86
rohitmj <rohit.maharjan619@gmail.com>
2017-01-11 12:17:13 +0545

    Add files via upload

---
+++ first and last digit of any digit number.c
@@ -0,0 +1,28 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer Fundamentals
+Roll no:
+Program : Write a program to print first and last digit of any digit number
+Lab Sheet No:15
+Date: 11th jan,2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,rem,i;
+    printf("Enter the four digit number\n");
+    scanf("%d",&n);
+    rem = n%10;
+    printf("The first digit of the number is %d\n",rem);
+    i= n/10;
+    while(i>=10)
+    {
+        i=i/10;
+    }
+    printf("The first digit of the number is %d",i);
+    getch();
+    return 0;
+}
+
+

9f914c03dac5129200521072727f9d8daa53fedb
rohitmj <rohit.maharjan619@gmail.com>
2017-01-12 18:14:59 +0545

    Update and rename count the first and last digit of any digit number using while loop.c to count the number of digits of any digit number using while loop.c

--- count the number of digits of any digit number using while loop.c
+++ count the number of digits of any digit number using while loop.c
@@ -2,7 +2,7 @@
 Student name: Rohit Maharjan
 Subject: Computer Fundamentals
 Roll no:
-Program : Write a program to print first and last digit of any digit number using while loop
+Program : Write a program to count the numbers of digits of any digit number using while loop
 Lab Sheet No:15
 Date: 11th jan,2017
 */
@@ -11,7 +11,7 @@ Date: 11th jan,2017
 int main()
 {
     int i,count=1;
-    printf("Enter the digit number\n");
+    printf("Enter the numbers\n");
     scanf("%d",&i);
     while(i>=10)
     {

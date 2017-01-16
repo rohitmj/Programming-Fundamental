2eb0a2568d751a3bf3de9394e20fac706dc7f623
rohitmj <rohit.maharjan619@gmail.com>
2017-01-16 12:18:05 +0545

    Add files via upload

---
+++ display.c
@@ -0,0 +1,28 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer Fundamentals
+Roll no: 30
+Program : Write a program to display the following
+           1234
+           5678
+Lab Sheet No:16
+Date: 16th jan,2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j,n=1;
+    for(i=1;i<=2;i++)
+    {
+        for(j=1;j<=4;j++)
+        {
+            printf("%d",n);
+            n++;
+        }
+        printf("\n");
+    }
+    getch();
+    return 0;
+
+}

1fe866f7f382377593b72ed1c59233dd9275ed5c
rohitmj <rohit.maharjan619@gmail.com>
2017-01-20 12:52:54 +0545

    Add files via upload

---
+++ Display the pattern2.c
@@ -0,0 +1,39 @@
+/*
+Student name: Rohit Maharjan
+Subject: Computer fundamentals
+Roll no : 30
+Program : Write a c program to display the patterns using if else
+						    xxxxx
+							 xxxx
+							  xxx
+							   xx
+							    x 
+Lab Sheet No : 19
+Date : 20 jan,2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{ 
+			
+		if(i>j)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("*");
+		
+		}
+
+		}
+		printf("\n");
+	}	
+	getch();
+	return 0;
+}
